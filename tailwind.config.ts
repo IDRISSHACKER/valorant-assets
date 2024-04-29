a2a36726b89c707efe9547a5c6ef37fcc560e127
IDRISSHACKER <idrisscoder@gmail.com>
2024-04-29 12:26:07 +0100

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        vForeground: "#0F1923",
+        vPrimary: "#FF4655",
+        vAppBg: "#ece8e1"
+      }
     },
   },
   plugins: [],
